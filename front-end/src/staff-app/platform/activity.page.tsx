1b23ccc94efdf5785bce48dce04fa11310eae83e
chrigglesby <chris@clicksuite.co.nz>
2023-07-03 11:21:26 +1200

    Load getActivities to activity page

--- front-end/src/staff-app/platform/activity.page.tsx
+++ front-end/src/staff-app/platform/activity.page.tsx
@@ -1,9 +1,30 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styled from "styled-components"
 import { Spacing } from "shared/styles/styles"
+import { useApi } from "shared/hooks/use-api"
+import { Activity } from "shared/models/activity"
 
 export const ActivityPage: React.FC = () => {
-  return <S.Container>Activity Page</S.Container>
+  const [getActivities, activitiesData, activitiesLoadState] = useApi<{ activity: Activity[] }>({ url: "get-activities" })
+
+  useEffect(() => {
+    void getActivities()
+  }, [getActivities])
+
+  console.log(activitiesData)
+
+  return <>
+    <S.Container>Activity Page</S.Container>
+    <h1>{ activitiesLoadState }</h1>
+    
+    {activitiesLoadState === "loaded" && activitiesData?.activity && (
+      <>
+        {activitiesData.activity.map((a) => (
+          <div>{ a.entity.id }</div>
+        ))}
+      </>
+    )}
+  </>
 }
 
 const S = {
